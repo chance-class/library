9bbe56e7f599ca9ef7b7ee953bbd26ee87bde81d
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-06-19 18:11:27 -0500

    Add onclick for button to display form

---
+++ script.js
@@ -0,0 +1,10 @@
+//let addBtn = document.getElementById("add-btn");
+//addBtn.addEventListener("click", () => {
+//  document.getElementById("form-popup").style.display = "block";
+//});
+
+function openForm() {
+  document.getElementById("form-popup").style.display = "block";
+}
+
+
