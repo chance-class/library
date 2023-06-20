44eae1aefff23bfe42d7616bbd39d07079e5cb4b
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-06-19 20:24:38 -0500

    Create Book constructor

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ function openForm() {
   document.getElementById("form-popup").style.display = "block";
 }
 
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+  }
+}
 
